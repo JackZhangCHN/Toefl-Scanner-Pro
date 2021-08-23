46ac69707890e6b2855e772cf2f42c56986aa541
JackZhangCHN <36500510+JackZhangCHN@users.noreply.github.com>
2021-08-23 20:54:38 +0800

    Update and rename Toefl.js to Toefl-revised.js
    +反复播放声音

--- Toefl-revised.js
+++ Toefl-revised.js
@@ -1,8 +1,8 @@
 //在这里加入你需要选择的城市。
-var city_choose = ["武汉","长沙","郑州"]
+var city_choose = ["杭州"]
 
 //在这里加入你需要选择的时间[start_time,end_time]。
-var time_start_end = ["2021-4-2","2021-10-1"]
+var time_start_end = ["2021-9-17","2021-9-19"]
 
 //搜索全部时间
 //var time_start_end = ["2000-4-10","2099-10-30"]
@@ -21,7 +21,6 @@ async function Scanner(city_choose, all) {
     //循环查找
     for (i = 1; i < city.options.length; ++i) {
         for(n = 0; n < city_choose.length; n++){
-            if(n==1&&all==true){break} //在全局搜索模式下，不再匹配city_choose里面的城市
             if(city_choose[n]==city.options[i].text||all==true){ //如果找到了指定的城市
                 city.options[i].selected = true
                 for (j = 1; j < day.options.length; ++j) {
@@ -38,9 +37,18 @@ async function Scanner(city_choose, all) {
                                         city.options[i].innerText,
                                         day.options[j].innerText,
                                         tb.rows[row].cells[1].innerText)
-}}}}}}}}}
+									
+										play()
+										for (countingg = 1; countingg <= 49; countingg++) {
+											play()
+											await sleep(1200);
+										}
+							
+										
+								}
+}}}}}}}}
 
-// 日期时间比对函数
+
 function compare(date){
     
     var cur = new Date(date.slice(0,date.indexOf("日")).replace("年","-").replace("月","-"));
@@ -50,9 +58,21 @@ function compare(date){
     return cur.getTime() >= start.getTime() && cur.getTime() <= end.getTime()
 }
 
+function play() {
+
+	var audio = new Audio('https://s1.thinktown.com/word/warning.mp3');
+	audio.play()
+	
+	
+
+}
 //搜索指定城市
-//Scanner(city_choose, false)
-
+async function go(){
+for (lloop = 1; lloop <= 9999; lloop++){
+	Scanner(city_choose, false)
+	await sleep(1*(60*1000))//1分钟一个循环
+}
+}
+ go()
 //搜索全部城市
-Scanner(city_choose, true)
-
+//Scanner(city_choose, true)
